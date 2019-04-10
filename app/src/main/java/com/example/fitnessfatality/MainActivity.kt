6313b2f1e6bbbac3151c290038d28f7ff59dea81
michalorestes <michalorestes@gmail.com>
2019-04-10 21:39:00 +0100

    Initial Set Up with data binding

--- app/src/main/java/com/example/fitnessfatality/MainActivity.kt
+++ app/src/main/java/com/example/fitnessfatality/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.fitnessfatality
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.fitnessfatality.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val atest: String = "This is something"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DataBindingUtil
+            .setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+            .apply {
+                this.lifecycleOwner = this@MainActivity
+                this.test = atest
+        }
     }
 }
