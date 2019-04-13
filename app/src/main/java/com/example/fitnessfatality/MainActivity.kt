d5e800abac32a832a9e4a2d4202a6d52ee7e0c80
michalorestes <michalorestes@gmail.com>
2019-04-13 10:43:48 +0100

    Bottom App Bar set up and navigation

--- app/src/main/java/com/example/fitnessfatality/MainActivity.kt
+++ app/src/main/java/com/example/fitnessfatality/MainActivity.kt
@@ -1,22 +1,19 @@
 package com.example.fitnessfatality
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.example.fitnessfatality.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val atest: String = "This is something"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DataBindingUtil
-            .setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-            .apply {
-                this.lifecycleOwner = this@MainActivity
-                this.test = atest
-        }
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        toolbar.setupWithNavController(navController)
+        navigation.setupWithNavController(navController)
     }
 }
