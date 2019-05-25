f4f4bd7429be08f05b9fe7721dda4d0d9c46fde3
Michal <michalorestes@gmail.com>
2019-05-25 18:20:27 +0100

    Display list of workouts

--- app/src/main/java/com/example/fitnessfatality/ui/myWorkouts/MyWorkoutsFragment.kt
+++ app/src/main/java/com/example/fitnessfatality/ui/myWorkouts/MyWorkoutsFragment.kt
@@ -11,17 +11,19 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fitnessfatality.R
 import com.example.fitnessfatality.ui.OnActivityInteractionInterface
+import com.example.fitnessfatality.ui.myWorkouts.adapters.WorkoutsListAdapter
 import com.example.fitnessfatality.ui.myWorkouts.viewModels.WorkoutViewModel
 import kotlinx.android.synthetic.main.fragment_my_workouts.*
 
 class MyWorkoutsFragment : Fragment() {
 
     private lateinit var workoutViewModel: WorkoutViewModel
-
     private lateinit var onActivityInterractionInterface: OnActivityInteractionInterface
+    private lateinit var recyclerViewAdapter: WorkoutsListAdapter
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -46,28 +48,28 @@ class MyWorkoutsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_my_workouts, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerViewAdapter = WorkoutsListAdapter()
+        val linearLayoutManager = LinearLayoutManager(context)
+        (workouts_list as RecyclerView).apply {
+            setHasFixedSize(true)
+            layoutManager = linearLayoutManager
+            adapter = recyclerViewAdapter
+        }
+    }
+
     override fun onStart() {
         super.onStart()
-
-        btn_click_me.setOnClickListener {
-            it.findNavController().navigate(R.id.workoutLoggingFragment)
-        }
-
-        btn_tester.setOnClickListener {
-            it.findNavController().navigate(R.id.action_myWorkoutsFragment_to_animationTesterFragment)
-        }
-
         onActivityInterractionInterface.setFabAction {
             Navigation
                 .findNavController(activity as Activity, R.id.workout_nav_host_fragment)
                 .navigate(R.id.action_myWorkoutsFragment_to_createNewWorkoutFragment)
         }
 
-        workoutViewModel.allWorkouts.observe(this, Observer {
+        this.workoutViewModel.allWorkouts.observe(this, Observer {
             if (it != null) {
-                it.forEach {
-                    Log.d("MyWorkouts->", it.name)
-                }
+                recyclerViewAdapter.updateDataSet(it)
             }
         })
     }
---
+++ app/src/main/java/com/example/fitnessfatality/ui/myWorkouts/adapters/WorkoutsListAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.fitnessfatality.ui.myWorkouts.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fitnessfatality.R
+import com.example.fitnessfatality.data.models.workout.Workout
+import kotlinx.android.synthetic.main.recycler_view_workout_list.view.*
+
+class WorkoutsListAdapter(): RecyclerView.Adapter<WorkoutsListAdapter.ViewHolder>(){
+    var dataSet: List<Workout> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemContainer = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_view_workout_list, parent, false) as ConstraintLayout
+
+        return ViewHolder(itemContainer)
+    }
+
+    fun updateDataSet(dataSet: List<Workout>) {
+        this.dataSet = dataSet
+        this.notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.listItemView.lb_title.text = dataSet[position].name
+    }
+
+    class ViewHolder(val listItemView: View): RecyclerView.ViewHolder(listItemView)
+}
