0254223e0213b444094bca60b5cb4db7c70c0d70
michalorestes <michalorestes@gmail.com>
2020-03-10 13:42:56 +0000

    Removed unused importr

--- app/src/main/java/com/example/fitnessfatality/ui/screens/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/fitnessfatality/ui/screens/dashboard/DashboardFragment.kt
@@ -1,7 +1,6 @@
 package com.example.fitnessfatality.ui.screens.dashboard
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
